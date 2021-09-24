7e1a039d1b0f6e8a7274342daf09236804a3d89a
Ben Altschuler <baltschuler@dhcp-10-250-109-144.harvard.edu>
2021-09-24 13:27:58 -0400

    Tyrant and std done

--- badstyrant.py
+++ badstyrant.py
@@ -111,30 +111,38 @@ class BadsTyrant(Peer):
                 self.dij[peer.id] = [self.up_bw/4, 0]
         if round > 0:
             uploaders = set()
-            for d in history.downloads[round-1]:
+            for d in history.downloads[-1]:
                 uploaders.add(d.from_id)
                 self.dij[d.from_id][0] = d.blocks
                 self.dij[d.from_id][1] += 1
                 if self.dij[d.from_id][1] >= self.r:
+                    print("BENJAMIN")
+                    print(self.uij[d.from_id])
                     self.uij[d.from_id] *= (1-self.lamb)
+                    print(self.uij[d.from_id])
+
 
             for peer in peers:
                 if peer.id not in uploaders and peer.id in self.history_requesters:
+                    print("DANIEL")
+                    print(peer.id, ": ", self.uij[peer.id])
                     self.uij[peer.id] *= (1+self.alpha)
                     self.dij[peer.id][1] = 0
+                    print(self.uij[peer.id])
 
         ordered_du = []
         for peer in peers:
             pid = peer.id
-            ordered_du.append([self.dij[pid][0] / self.uij[pid], self.uij[pid], pid])
+            if "Seed" not in pid:
+                ordered_du.append([self.dij[pid][0] / self.uij[pid], self.uij[pid], pid, self.dij[pid][0]])
         ordered_du.sort(reverse=True)
+        print(ordered_du)
         summation = 0
         i = 0
         while summation < self.cap and i < len(ordered_du):
             chosen.append(Upload(self.id, ordered_du[i][2], min(self.cap-summation, ordered_du[i][1])))
             summation += ordered_du[i][1]
             i += 1
-        
 
         return chosen
 
